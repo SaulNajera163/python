af6fbe360ef86257a73f3a33afac402ffe020a54
Saul Najera <saulnajera163@gmail.com>
2025-08-05 10:09:01 -0600

    At home before getting job

--- a2_flow_control/a2_exercise.py
+++ a2_flow_control/a2_exercise.py
@@ -1,7 +1,5 @@
 import os
-
 os.system("cls")
-
 # print("----------------First----------------")
 
 # a = int(input("Enter A: "))
@@ -14,45 +12,49 @@ os.system("cls")
 
 print("----------------Second----------------")
 
-print("Welcome to our Calculation")
-a = int(input("Enter A: "))
-b = int(input("Enter B: "))
+# print("Welcome to our Calculation")
+# a = int(input("Enter A: "))
+# b = int(input("Enter B: "))
 
-option = int(input("\n1.-Plus \n2.-Minus \n3.-Divide by \n4.-Product \n5.-Exit" + "\n"))
+# option = int(input("\n1.-Plus \n2.-Minus \n3.-Divide by \n4.-Product \n5.-Exit" + "\n"))
 
-if option == 1:
-    print(f"Plus: {a} + {b} is equals -> {a + b}")
+# if option == 1:
+#     print(f"Plus: {a} + {b} is equals -> {a + b}")
 
-elif option == 2:
-    print(f"Minus: {a} - {b} is equals -> {a - b}")
+# elif option == 2:
+#     print(f"Minus: {a} - {b} is equals -> {a - b}")
 
-elif option == 3:
-    if b == 0:
-        print("We can't divide by Zero!!!!")
-    else:
-        print(f"Divide: {a} by {b} is equals -> {a / b}")
+# elif option == 3:
+#     if b == 0:
+#         print("We can't divide by Zero!!!!")
+#     else:
+#         print(f"Divide: {a} by {b} is equals -> {a / b}")
 
-elif option == 4:
-    print(f"Product: {a} * {b} is equals -> {a * b}")
-else:
-    print("Good bye")
+# elif option == 4:
+#     print(f"Product: {a} * {b} is equals -> {a * b}")
+# else:
+#     print("Good bye")
 
 
 print("----------------Three----------------")
 
-# Ejercicio 3: Año bisiesto
-# Pide al usuario que introduzca un año y determina si es bisiesto.
-# Un año es bisiesto si es divisible por 4, excepto si es divisible por 100 pero no por 400.
 
-year = int(input("Enter the year: "))
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    #(anio % 4 == 0) and ((anio % 100 != 0) or (anio % 400 == 0))
-    print(f"Year {year} is busiest ")
+# year = int(input("Enter the year: "))
+# if(year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)):
+#     print(f"Year {year} is busiest ")
+# else:
+#     print("It's nos busiest")
 
-# Ejercicio 4: Categorizar edades
-# Pide al usuario que introduzca una edad y la clasifique en:
-# - Bebé (0-2 años)
-# - Niño (3-12 años)
-# - Adolescente (13-17 años)
-# - Adulto (18-64 años)
-# - Adulto mayor (65 años o más)
+print("----------------Four----------------")
+
+age = int(input("Enter your age: "))
+if age >= 0 and age <= 2:
+    print("Baby")
+elif age >= 3 and age <= 12:
+    print("Boy/Girl")
+elif age >= 13 and age <= 17:
+    print("Teenager")
+elif age >= 18 and age <= 64:
+    print("Sr. or Sra.")
+else:
+    print("Old Sr. or Sra.")
